9149c73abd51a16792f584c3fcd6b18f2ea293eb
pankajshah13 <pankajshah143.ps@gmail.com>
2019-06-19 02:10:33 +0530

    change routing loginpage to home page

--- src/app/login/app-routing.module.ts
+++ src/app/login/app-routing.module.ts
@@ -5,14 +5,14 @@ import { AboutComponent } from '../about/about.component';
 import { HomeComponent } from '../home/home.component';
 // import { ContactComponent } from './contact/contact.component';
 import { ResumeComponent } from '../resume/resume.component';
-// import { LoginComponent } from './login/login.component';
 import { MainComponent } from '../main/main.component';
+import { LoginComponent } from './login.component';
 const routes: Routes = [
 
 
 
-  { path: '', redirectTo: 'login', pathMatch: 'full' },
-  // { path: 'login', component: LoginComponent },
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'login', component: LoginComponent },
 
   {
     path: 'home', component: MainComponent, children: [
